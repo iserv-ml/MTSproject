bd78ea69d905f9b0db86b4109801635dc4b26943
d.sissoko <d.sissoko@DRPEPR0667.orangemali.local>
2020-01-10 09:48:22 +0000

    Aiguillage spécial

--- src/AppBundle/Controller/VehiculeController.php
+++ src/AppBundle/Controller/VehiculeController.php
@@ -232,8 +232,8 @@ class VehiculeController extends Controller
         if ($this->get('security.authorization_checker')->isGranted('ROLE_ENREGISTREMENT')){
                 $action .= " <a title='Modifier' class='btn btn-info' href='".$this->generateUrl('vehicule_edit', array('id'=> $id ))."'><i class='fa fa-edit' ></i></a>";
                 $action .= " <a title='Supprimer' class='btn btn-danger' href='".$this->generateUrl('vehicule_delete_a', array('id'=> $id ))."' onclick='return confirm(\"Confirmer la suppression?\")'><i class='fa fa-trash-o'> </i></a>";
-                $action .= " <a title='Aiguiller' class='btn btn-warning' href='".$this->generateUrl('aiguiller', array('id'=> $id, 'type'=>0 ))."' ><i class='fa fa-compass'> </i></a>";
-                $action .= " <a title='Aiguiller' class='btn btn-warning' href='".$this->generateUrl('aiguiller', array('id'=> $id, 'type'=>1 ))."' ><i class='fa fa-compass'> </i></a>";
+                $action .= " <a title='Aiguiller normal' class='btn btn-warning' href='".$this->generateUrl('aiguiller', array('id'=> $id, 'type'=>0 ))."' ><i class='fa fa-compass'> </i></a>";
+                $action .= " <a title='Aiguiller spécial' class='btn btn-djib' href='".$this->generateUrl('aiguiller', array('id'=> $id, 'type'=>1 ))."' ><i class='fa fa-compass'> </i></a>";
         }
         
         if ($this->get('security.authorization_checker')->isGranted('ROLE_SUPERVISEUR')){
