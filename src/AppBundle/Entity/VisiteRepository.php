54ddba79bb5c136a90fc08f77db3c4c0d354b843
d.sissoko <d.sissoko@DFADSECPR0667.orangemali.local>
2022-11-24 15:53:03 +0000

    Ajout du genre sur la page de recherche du vehicule pour les menus controle et délivrance

--- src/AppBundle/Entity/VisiteRepository.php
+++ src/AppBundle/Entity/VisiteRepository.php
@@ -301,7 +301,7 @@ class VisiteRepository extends EntityRepository
         try{
             $controle = ($piste == 0) ? 'pi.numero > :piste ' : 'pi.numero = :piste ';
             $conn = $this->getEntityManager()->getConnection();
-            $sql = 'SELECT r.id, r.contre_visite, r.contre_visite_visuelle,r.immatriculation_v, v.immatriculation, v.type_chassis, v.chassis, r.revisite, r.statut, p.nom, p.prenom,pi.numero as piste, v.id as vehicule FROM visite r LEFT JOIN vehicule v ON r.vehicule_id = v.id LEFT JOIN proprietaire p ON v.proprietaire_id = p.id LEFT JOIN chaine c on r.chaine_id = c.id LEFT JOIN piste pi on c.piste_id = pi.id '
+            $sql = 'SELECT r.id, r.contre_visite, r.contre_visite_visuelle,r.immatriculation_v, v.immatriculation, v.type_chassis, v.chassis, r.revisite, r.statut, p.nom, p.prenom,pi.numero as piste, v.id as vehicule, g.code as genre FROM visite r LEFT JOIN vehicule v ON r.vehicule_id = v.id LEFT JOIN proprietaire p ON v.proprietaire_id = p.id LEFT JOIN chaine c on r.chaine_id = c.id LEFT JOIN piste pi on c.piste_id = pi.id LEFT JOIN typevehicule tp on v.type_vehicule_id = tp.id LEFT JOIN genre g on tp.genre_id = g.id'
                         . ' WHERE r.statut IN (1,2, 3) AND v.immatriculation like :immatriculation AND '.$controle;
             
             $stmt = $conn->prepare($sql);
@@ -317,7 +317,7 @@ class VisiteRepository extends EntityRepository
     public function findDelivranceAjaxAlleger($immatriculation) {
         try{
             $conn = $this->getEntityManager()->getConnection();
-            $sql = 'SELECT r.id, r.contre_visite, r.contre_visite_visuelle, r.dateControle, v.immatriculation, v.type_chassis, v.chassis, r.revisite, r.statut, p.nom, p.prenom,pi.numero as piste, ca.numero as caisse FROM visite r LEFT JOIN vehicule v ON r.vehicule_id = v.id LEFT JOIN proprietaire p ON v.proprietaire_id = p.id LEFT JOIN chaine c ON r.chaine_id = c.id LEFT JOIN piste pi ON c.piste_id = pi.id LEFT JOIN caisse ca ON c.caisse_id = ca.id'
+            $sql = 'SELECT r.id, r.contre_visite, r.contre_visite_visuelle, r.dateControle, v.immatriculation, v.type_chassis, v.chassis, r.revisite, r.statut, p.nom, p.prenom,pi.numero as piste, ca.numero as caisse, g.code as genre FROM visite r LEFT JOIN vehicule v ON r.vehicule_id = v.id LEFT JOIN proprietaire p ON v.proprietaire_id = p.id LEFT JOIN chaine c ON r.chaine_id = c.id LEFT JOIN piste pi ON c.piste_id = pi.id LEFT JOIN caisse ca ON c.caisse_id = ca.id LEFT JOIN typevehicule tp on v.type_vehicule_id = tp.id LEFT JOIN genre g on tp.genre_id = g.id'
                         . ' WHERE r.statut IN (2,3,4) AND v.immatriculation like :immatriculation ';
             $stmt = $conn->prepare($sql);
             $stmt->execute(array('immatriculation' => "%".$immatriculation."%"));
